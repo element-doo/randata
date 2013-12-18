c381e5145ce43ea75ad5e2c6ed5782560be31592
Tomo Ferega <tomo@element.hr>
2013-12-18 15:40:19 +0100

    Fixed bug with phones. Version bump.

--- code/Extensions-People/src/main/scala/net/randata/people/Phones.scala
+++ code/Extensions-People/src/main/scala/net/randata/people/Phones.scala
@@ -3,8 +3,8 @@ package people
 
 trait Phones { self: Randata =>
   def nextPhone = {
-    val firstGroup  = (1 to 3).map(e => nextDigit).mkString
-    val secondGroup = (1 to 4).map(e => nextDigit).mkString
+    val firstGroup  = (1 to 3).map(e => nextDigit()).mkString
+    val secondGroup = (1 to 4).map(e => nextDigit()).mkString
     s"555 $firstGroup-$secondGroup"
   }
 }
